47321c1026ab33ca252e90091d9df8ee6f2aa756
Rimbaud Gregoire <rimbaudgregoire@hotmail.be>
2019-09-13 09:37:37 +0200

    Done with DB access.

--- src/client/app.js
+++ src/client/app.js
@@ -2,12 +2,16 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
+//////////////////////////////////////////// Location ////////////////////////////////////////////
+
 function showPosition(position) {
     const radianLat = (position.coords.latitude * Math.PI) / 180;
     const radianLon = (position.coords.longitude * Math.PI) / 180;
+    const location = `${radianLat} | ${radianLon}`;
+
+    ReactDOM.render(location, document.querySelector("#app"));
 
     // let math = Math.acos(Math.sin(radianLat) * Math.sin(0) + Math.cos(radianLat) * Math.cos(0) * Math.cos(0 - radianLon)) * 6371;
-    document.querySelector("#demo").innerHTML = `${radianLat} | ${radianLon}`;
 }
 function getLocation() {
     if (navigator.geolocation) {
@@ -17,5 +21,3 @@ function getLocation() {
     }
 }
 getLocation();
-
-ReactDOM.render(document.querySelector("#app"));
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import path from "path";
+import router from "./api-routes";
 
 const mongo = require("mongodb").MongoClient;
-
 const {APP_PORT} = process.env;
-
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
@@ -21,15 +20,17 @@ mongo.connect("mongodb://dev:dev@mongo:27017/admin", (err, client) => {
     const terminals = db.collection("terminals");
     const banks = db.collection("banks");
 
-    terminals
-        .find({address: "Zeelaan 67, 8670 Koksijde"})
-        .toArray((err, item1) => {
-            // eslint-disable-next-line no-console
-            const thatBank = item1[0].bank;
+    // terminals
+    //     .find({address: "Zeelaan 67, 8670 Koksijde"})
+    //     .toArray((err, item1) => {
+    //         // eslint-disable-next-line no-console
+    //         const thatBank = item1[0].bank;
 
-            banks.find({_id: thatBank}).toArray((err, item2) => {
-                // eslint-disable-next-line no-console
-                console.log(item2[0].name);
-            });
-        });
+    //         banks.find({_id: thatBank}).toArray((err, item2) => {
+    //             // eslint-disable-next-line no-console
+    //             console.log(item2[0].name);
+    //         });
+    //     });
 });
+
+app.use("/api", router);
